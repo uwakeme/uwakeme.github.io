ba131ebecf94bc707a51920d05a2759492011190
uwakeme <lb1982388831@163.com>
2025-03-21 11:41:54 +0800

    备份Hexo代码

--- 同步git代码.sh
+++ 同步git代码.sh
@@ -3,7 +3,7 @@ git commit -m "备份Hexo代码"
 echo "------------"
 echo "提交Hexo代码"
 echo "------------"
-git push origin hexo
+git push origin hexo -v
 echo "------------"
 echo "推送Hexo代码"
 echo "------------"
